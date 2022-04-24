64803dd3afecb52ada675a36c70d6e7ad69c27b3
HIM7707 <singhhimank400@gmail.com>
2022-04-24 16:56:08 +0530

    full code for graph impl part.1

--- GRAPHS/graphs.cpp
+++ GRAPHS/graphs.cpp
@@ -1,21 +1,51 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
-int main()
-{
-    int a = 5, b = 10, temp;
+class Graph{
+    // variables
+    int V;
+    list<int> *L;
+    // now member functions
+public:
+    Graph(int v){
+        V=v;
+        L = new list<int>[V];
+    }
+    
+    void addEdge(int u,int v,bool bidir = true){
+        L[u].push_back(v);
+        if (bidir == true){
+            L[v].push_back(u);
+        }
+    }
+    void print_the_graph(){
+        for (int i=0;i<V;i++){
+            cout<<i<<"->";
+            for(int vertex:L[i]){
+                cout<<vertex<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
 
-    cout << "Before swapping." << endl;
-    cout << "a = " << a << ", b = " << b << endl;
 
-    temp = a;
-    a = b;
-    b = temp;
-    // hi my name is himank.
-    // hi
 
-    cout << "\nAfter swapping." << endl;
-    cout << "a = " << a << ", b = " << b << endl;
 
-    return 0;
+int main() {
+	Graph g(5);
+	g.addEdge(0,1);
+	g.addEdge(0,4);
+	g.addEdge(4,3);
+	g.addEdge(1,4);
+	g.addEdge(1,2);
+	g.addEdge(2,3);
+	g.addEdge(1,3);
+	
+	g.print_the_graph();
+	return 0;
+      
+
+    
 }
