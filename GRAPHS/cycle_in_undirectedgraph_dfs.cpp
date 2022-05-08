2956d80c272f0d249c4627d6639453a97ee94345
HIM7707 <singhhimank400@gmail.com>
2022-05-09 00:33:35 +0530

    dfs cycle detect undirected

---
+++ GRAPHS/cycle_in_undirectedgraph_dfs.cpp
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+  
+    bool cycleDFS(int curr,int pre, vector <bool> & visited, vector <int>adj[]){
+        visited[curr] = true;
+        // traversion over neighbours
+        for(int a:adj[curr]){
+            if(visited[a]==false){
+                if(cycleDFS(a,curr,visited,adj)) return true;   
+            } 
+            // best move of the code -> only at the end of recursion a!= pre + both are visited
+            else if(a!=pre) return true;
+        }
+        return false;
+    }  
+  
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        vector <bool> visited(V,false);
+        // We use a loop to access every component of a graph.
+        for(int i=0;i<V;i++){
+            if(visited[i]==false){
+                if(cycleDFS(i,-1,visited,adj)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}  // } Driver Code Ends
