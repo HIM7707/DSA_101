3f4bc0d520e8b4e8d9a140a7889f626e1d9d9bb2
Himank Singh <79526149+HIM7707@users.noreply.github.com>
2023-09-04 07:58:56 +0530

    Update inordertraversal.cpp

--- Segment_Tree/inordertraversal.cpp
+++ Segment_Tree/inordertraversal.cpp
@@ -5,12 +5,24 @@ vector<int> levelOrder(Node* node)
       if(node == NULL) return ans;
       queue <Node*> Q;
       Q.push(node);
+      Q.push(NULL);  
       
       while(!Q.empty()){
           // access the front element
           // push its children
           // pop the front element
           Node * curr = Q.front();
+          if(curr == NULL){
+              cout<<endl;
+              Q.pop();
+              if(!Q.empty()){
+                  Q.push(NULL);
+              }
+          }
+          else{
+               
+          Q.pop();
+          
           ans.push_back(curr->data);
          // cout<<curr->data<<" ";
           if(curr->left != NULL){
@@ -19,6 +31,7 @@ vector<int> levelOrder(Node* node)
           if(curr->right != NULL){
               Q.push(curr->right);
           }
+          }   
               
 
           
