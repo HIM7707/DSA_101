797098f4e5e3489fcb0d92f8d505e70928976d65
Himank Singh <79526149+HIM7707@users.noreply.github.com>
2023-09-22 10:18:34 +0530

    Create max_diff.cpp

---
+++ arrays/max_diff.cpp
@@ -0,0 +1,15 @@
+
+int maxDiff(int arr[], int n)
+{
+	int res = arr[1] - arr[0], minVal = arr[0];
+
+	for(int i = 1; i < n; i++)
+	{
+		
+			res = max(res, arr[i] - minVal);
+			
+			minVal = min(minVal, arr[i]);
+	}
+
+	return res;
+}
